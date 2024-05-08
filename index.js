2da47e09615a01707399845f30171662c01342bd
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-08 20:41:33 +0700

    Set proper docker config path. Removed unnecessary exports.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import chalk from "chalk";
 import { execSync } from "child_process";
 import commandExists from "command-exists";
 import { Command } from "commander";
+import path from "node:path";
+import { fileURLToPath } from "url";
 
 const APP_NAME = "Local Sui Explorer";
 const APP_URL = "http://localhost:9001";
 
-export const main = async () => {
+const main = async () => {
   // Make sure Docker is installed.
   await checkDocker();
 
@@ -16,13 +18,15 @@ export const main = async () => {
 
   program.name("sui-explorer-local").description(`Easily manage ${APP_NAME}`);
 
+  const cliDirectory = getCliDirectory();
+
   program
     .command("start")
     .description(`Start ${APP_NAME}`)
     .option("-v, --verbose", "display logs")
     .action((options) => {
       performAction(
-        "docker compose up -d",
+        `docker compose -f ${cliDirectory}/compose.yml up -d`,
         `${APP_NAME} started on ${APP_URL}`,
         `Could not start ${APP_NAME}`,
         options.verbose
@@ -35,7 +39,7 @@ export const main = async () => {
     .option("-v, --verbose", "display logs")
     .action((options) => {
       performAction(
-        "docker compose down",
+        `docker compose -f ${cliDirectory}/compose.yml down`,
         `${APP_NAME} stopped`,
         `Could not stop ${APP_NAME}`,
         options.verbose
@@ -48,7 +52,7 @@ export const main = async () => {
     .option("-v, --verbose", "display logs")
     .action((options) => {
       performAction(
-        "docker compose up -d --force-recreate --build",
+        `docker compose -f ${cliDirectory}/compose.yml up -d --force-recreate --build`,
         `${APP_NAME} rebuilt on ${APP_URL}`,
         `Could not rebuild ${APP_NAME}`,
         options.verbose
@@ -58,7 +62,7 @@ export const main = async () => {
   program.parse();
 };
 
-export const checkDocker = async () => {
+const checkDocker = async () => {
   try {
     await commandExists("docker");
   } catch {
@@ -69,12 +73,12 @@ export const checkDocker = async () => {
   }
 };
 
-export const performAction = (
-  command,
-  successMessage,
-  errorMessage,
-  verbose
-) => {
+const getCliDirectory = () => {
+  const currentFileUrl = import.meta.url;
+  return path.dirname(decodeURI(fileURLToPath(currentFileUrl)));
+};
+
+const performAction = (command, successMessage, errorMessage, verbose) => {
   const ignore = !verbose ? "ignore" : undefined;
 
   try {
@@ -90,11 +94,11 @@ export const performAction = (
   displaySuccessMessage(successMessage);
 };
 
-export const displayErrorMessage = (message) => {
+const displayErrorMessage = (message) => {
   console.error(chalk.red(message));
 };
 
-export const displaySuccessMessage = (message) => {
+const displaySuccessMessage = (message) => {
   console.log(chalk.green(message));
 };
 
